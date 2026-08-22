4c9e6a76533d0e40a5a837609ef03e0cdba9e54e
Ankita2005899 <ankitabandal45@gmail.com>
2026-08-22 07:17:17 +0530

    Consolidate project into 5 core files

--- public/js/utils/dom.js
+++
@@ -1,7 +0,0 @@
-function escapeHtml(str) {
-  return String(str == null ? '' : str).replace(/[&<>"']/g, c => ({
-    '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;'
-  }[c]));
-}
-
-export { escapeHtml };
--- server/routes/maintenance.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const maintenance = require('../controllers/maintenance.controller');
-const { requireSecretary } = require('../middleware/auth');
-
-router.get('/', requireSecretary, maintenance.byMonth);
-router.post('/', requireSecretary, maintenance.save);
-
-module.exports = router;
